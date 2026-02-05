1005ad1b6cb171235d90bc24e6ec60c81b87f2c1
pnjia <panjiangkasaputra67@gmail.com>
2026-02-05 16:27:39 +0700

    feat: create Transaction model with fillable attributes and relationships

---
+++ LARAVEL_Projects/ecommerce-dashboard/app/Models/Transaction.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\BelongsToTenant;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    //
+    use HasUuids, BelongsToTenant;
+
+    protected $fillable = [
+        'tenant_id',
+        'user_id',
+        'invoice_code',
+        'transaction_date',
+        'total_amount',
+        'payment_amount',
+        'change_amount'
+    ];
+
+    protected $casts = [
+        'transaction_date' => 'datetime',
+        'total_amount' => 'decimal:2',
+        'payment_amount' => 'decimal:2',
+        'change_amount' => 'decimal:2',
+    ];
+
+    public function details()
+    {
+        return $this->hasMany(TransactionDetail::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
