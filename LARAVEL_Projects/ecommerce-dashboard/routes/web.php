003cedb6dff9c2bf598ed64a25ab19911d4c27fa
pnjia <panjiangkasaputra67@gmail.com>
2026-02-05 16:26:56 +0700

    feat: add transaction routes and update dashboard route in web.php

--- LARAVEL_Projects/ecommerce-dashboard/routes/web.php
+++ LARAVEL_Projects/ecommerce-dashboard/routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -20,6 +22,15 @@ Route::middleware('auth')->group(function () {
 
     Route::resource('categories', CategoryController::class);
     Route::resource('products', ProductController::class);
+
+    Route::get('/pos', [TransactionController::class, 'create'])->name('transactions.create');
+    Route::post('/pos', [TransactionController::class, 'store'])->name('transactions.store');
+
+    Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions.index');
+    Route::get('/transactions/{transaction}', [TransactionController::class, 'show'])->name('transactions.show');
+
+    Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
