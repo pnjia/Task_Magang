5f86f6a50b03284015418820c710d52f3191ff65
pnjia <panjiangkasaputra67@gmail.com>
2026-02-05 10:22:03 +0700

    feat: add product resource routes to web.php

--- LARAVEL_Projects/ecommerce-dashboard/routes/web.php
+++ LARAVEL_Projects/ecommerce-dashboard/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +19,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::resource('categories', CategoryController::class);
+    Route::resource('products', ProductController::class);
 });
 
 require __DIR__.'/auth.php';
