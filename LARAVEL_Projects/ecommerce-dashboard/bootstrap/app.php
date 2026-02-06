57b038f6f3b2759193ffd3361de15dd34a7bd2ac
pnjia <panjiangkasaputra67@gmail.com>
2026-02-06 16:28:03 +0700

    feat: add CheckRole middleware alias to middleware configuration for role-based access control

--- LARAVEL_Projects/ecommerce-dashboard/bootstrap/app.php
+++ LARAVEL_Projects/ecommerce-dashboard/bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\CheckRole;
 use App\Http\Middleware\SetupTenantContext;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -7,14 +8,18 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
         $middleware->web(append: [
             SetupTenantContext::class
+        ]);
+
+        $middleware->alias([
+            'role' => CheckRole::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
