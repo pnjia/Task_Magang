2089d1a627c0a36708184c5ae7c9a51e8ebc18f8
pnjia <panjiangkasaputra67@gmail.com>
2026-02-10 17:01:57 +0700

    feat: update Tailwind and Vite configurations to support JSX files and enhance React integration

--- LARAVEL_Projects/ecommerce-dashboard/vite.config.js
+++ LARAVEL_Projects/ecommerce-dashboard/vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
+        react({
+            jsxRuntime: 'automatic',
+        }),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.jsx'],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '@': '/resources/js',
+        },
+    },
 });
