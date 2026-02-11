9db76c666dfcc892bb26dc42400d82c1b49fb3c6
pnjia <panjiangkasaputra67@gmail.com>
2026-02-11 15:54:11 +0700

    feat(backend): update package-lock.json dan package.json untuk dependencies terbaru

--- LARAVEL_Projects/ecommerce-dashboard/resources/views/app.blade.php
+++ LARAVEL_Projects/ecommerce-dashboard/resources/views/app.blade.php
@@ -12,8 +12,8 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
-    @routes
     @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+    @routes
     @inertiaHead
 </head>
 
--- LARAVEL_Projects/ecommerce-dashboard/vite.config.js
+++ LARAVEL_Projects/ecommerce-dashboard/vite.config.js
@@ -4,15 +4,20 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
-        react(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.jsx'],
             refresh: true,
         }),
+        react(),
     ],
     resolve: {
         alias: {
             '@': '/resources/js',
+        },
+    },
+    server: {
+        hmr: {
+            host: 'localhost',
         },
     },
 });
